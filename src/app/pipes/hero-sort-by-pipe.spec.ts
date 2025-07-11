598d69ba3cc07c23b18d5a21d4a928971a31993a
juan manuel <jmgv87191@gmail.com>
2025-07-11 11:56:24 -0700

    asd

---
+++ src/app/pipes/hero-sort-by-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { HeroSortByPipe } from './hero-sort-by-pipe';
+
+describe('HeroSortByPipe', () => {
+  it('create an instance', () => {
+    const pipe = new HeroSortByPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
